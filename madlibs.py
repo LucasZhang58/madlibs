2b08f9479f8de68ac7006df1fbf0ae03fb38060e
LucasZhang58 <49825950+LucasZhang58@users.noreply.github.com>
2020-12-13 15:14:29 -0500

    Add files via upload
    madlibs file upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to _____"
+youtuber = "Lucas Zhang"
+
+# a few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
